45110c1dba431f79b86927e1e89b32110280a397
Guilherme Henrique <59860892+ghpm99@users.noreply.github.com>
2020-01-15 13:14:37 -0300

    Bot

---
+++ src/main/java/br/com/bot/kawori/dao/Dao.java
@@ -0,0 +1,18 @@
+package br.com.bot.kawori.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+
+	Optional<T> get(long id);
+	
+	List<T> getAll();
+	
+	void save(T t);
+	
+	void update(T t, String[] params);
+	
+	void delete(T t);
+	
+}
---
+++ src/main/java/br/com/bot/kawori/dao/UsuarioDao.java
@@ -0,0 +1,35 @@
+package br.com.bot.kawori.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import br.com.bot.kawori.model.Usuario;
+
+public class UsuarioDao implements Dao<Usuario> {
+
+	public Optional<Usuario> get(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Usuario> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void save(Usuario t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(Usuario t, String[] params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(Usuario t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
