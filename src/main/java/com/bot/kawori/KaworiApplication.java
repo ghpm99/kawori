f82771b1ce0f746ca335321661705ed120b4d7b5
Guilherme Henrique <59860892+ghpm99@users.noreply.github.com>
2020-01-15 02:26:20 -0300

    Spring boot

---
+++ src/main/java/com/bot/kawori/KaworiApplication.java
@@ -0,0 +1,13 @@
+package com.bot.kawori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaworiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaworiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bot/kawori/KaworiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.kawori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaworiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
