96d8385fd9f5dd4c1dbdaf124dc3715cb7215ba7
Guilherme Henrique <59860892+ghpm99@users.noreply.github.com>
2021-05-13 11:27:00 -0300

    Main

--- src/main/java/com/bot/KaworiSpring/Main.java
+++ src/main/java/com/bot/KaworiSpring/Main.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class KaworiSpringApplication{
+public class Main{
 	
 	public static void main(String[] args) {		
-		SpringApplication.run(KaworiSpringApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
